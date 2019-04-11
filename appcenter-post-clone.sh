25ea5ff17677752cb033818aa09c29f07a6690ab
Tibor Bödecs <mail.tib@gmail.com>
2019-04-11 23:39:38 +0200

    post-clone 2

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-gem install cocoapods --pre
+echo "-----------------------------------------"
+echo "Uninstalling all CocoaPods versions"
+sudo gem uninstall cocoapods --all --executables
+echo "Installing CocoaPods version $COCOAPODS_VER"
+sudo gem install cocoapods --pre
+echo "-----------------------------------------"
